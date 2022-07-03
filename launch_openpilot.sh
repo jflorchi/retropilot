31102972f3050d82d4c822e40e8ffc20ee10246b
jflorchi <jordan.florchinger@alumni.uleth.ca>
2022-07-03 09:55:44 -1100

    progress

--- launch_openpilot.sh
+++ launch_openpilot.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/bash
 
 export PASSIVE="0"
-export LD_LIBRARY_PATH="/vendor/lib64:/system/lib64:$LD_LIBRARY_PATH"
+export LD_LIBRARY_PATH="/data/openpilot/third_party/android_libs:/vendor/lib64:/system/lib64:$LD_LIBRARY_PATH"
 export LD_LIBRARY_PATH="/data/openpilot/third_party/snpe/aarch64:$LD_LIBRARY_PATH"
 export LOGPRINT="debug"
 exec ./launch_chffrplus.sh
--- launch_uiview.sh
+++ launch_uiview.sh
@@ -4,5 +4,6 @@ export LD_LIBRARY_PATH="/vendor/lib64:/system/lib64:$LD_LIBRARY_PATH"
 export LD_LIBRARY_PATH="/data/openpilot/third_party/snpe/aarch64:$LD_LIBRARY_PATH"
 ln -sfn $(pwd) /data/pythonpath
 export PYTHONPATH="$PWD:$PWD/pyextra"
+export LOGPRINT="debug"
 
 selfdrive/debug/uiview.py
